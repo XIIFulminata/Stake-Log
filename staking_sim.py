436702ec931c08ab3e2d4fabbe7537f85a46efd2
jovaun jackson <jovaun@dropbox.com>
2019-11-06 15:47:16 -0800

    make script take sys args

--- staking_sim.py
+++ staking_sim.py
@@ -1,7 +1,8 @@
+import sys
+
 from random import randint
 
 class StakingSimulation(object):
-    # TODO make stake amount take a dict (JSON) as well to allow for multiple outputs
 
     def __init__(self, stake_amount: int, sim_length: int = 10000):
         self.stake_amount = stake_amount
@@ -19,6 +20,7 @@ class StakingSimulation(object):
         cash -- amount of gold in Mgp that the player has to stake
         target -- minimum amount of gold in Mgp that the player wants to leave with
         """
+        # print(f"tried to run helper with these params cash: {cash}, target: {target}")
         if cash >= target:
             self.stake_count += self.stake_num
             return True
@@ -35,6 +37,9 @@ class StakingSimulation(object):
         return self._run_sim_helper(cash, target)
 
     def run_sim(self, cash: int, target: int):
+        print (f"Starting cash: {cash}M gp")
+        print (f"Target cash: {target}M gp")
+        print (f"Running sim {self.sim_length} times with a base stake of {self.stake_amount}")
         for _ in range(self.sim_length):
             self.stake_num = 0
             self.multiplier = 1
@@ -44,8 +49,8 @@ class StakingSimulation(object):
                 self.losses += 1
         print (f"{self.wins} Wins, {self.losses} Losses")
         print (f"Win rate: {round(self.wins/self.sim_length*100, 2)}%")
-        print (f"Average stakes to win: {round(self.stake_count/self.wins)} stake")
+        print (f"Average stakes to win: {round(self.stake_count/self.wins)} stakes")
 
 if __name__ == "__main__":
-    # TODO take sys args for the sim
-    StakingSimulation(7).run_sim(500, 3000)
+    stake_amount, cash, target = [int(x) for x in sys.argv[1:4]]  # TODO use inputs instead
+    sim = StakingSimulation(stake_amount).run_sim(cash, target)
