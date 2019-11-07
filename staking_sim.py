7dcf1224afd3cb894f55efaa9c51244ebf09e3f2
jovaun jackson <jovaun@dropbox.com>
2019-11-06 16:26:09 -0800

    add helper to provide better error messages

--- staking_sim.py
+++ staking_sim.py
@@ -1,21 +1,36 @@
 import sys
 
 from random import randint
+from typing import Optional
 
 class StakingSimulation(object):
 
+    @staticmethod
+    def _get_input_or_default(input_message: str, default_value: Optional[int] = None) -> int:
+        """
+        Returns a user input or default value.
+        Retry until an appropriate value is given (unless a default is provided)
+        """
+        while True:
+            try:
+                return int(input(input_message))
+            except ValueError:
+                if default_value != None:
+                    print(f"Using default value of {default_value}")
+                    return default_value
+                else:
+                    print("Please provide a valid integer input")
+
+
     def __init__(self):
-        try:
-            self.stake_amount = int(input('Enter your base stake? (Default 10M): '))
-        except ValueError:
-            self.stake_amount = 10
-            print("Using default value of 10")
-        
-        try:
-            self.sim_length = int(input('How many simulations should be run? (Default 10,000): '))
-        except ValueError:
-            self.sim_length = 10000
-            print("Using default value of 10,000")
+        self.stake_amount = StakingSimulation._get_input_or_default(
+            input_message='Enter your base stake? (Default 10M): ',
+            default_value=10
+        )
+        self.sim_length = StakingSimulation._get_input_or_default(
+            input_message='How many simulations should be run? (Default 10,000): ',
+            default_value=10000
+        )
 
         self.tax_rate = 0.99
         self.wins = 0
@@ -30,13 +45,12 @@ class StakingSimulation(object):
         cash -- amount of gold in Mgp that the player has to stake
         target -- minimum amount of gold in Mgp that the player wants to leave with
         """
-        # print(f"tried to run helper with these params cash: {cash}, target: {target}")
         if cash >= target:
             self.stake_count += self.stake_num
             return True
         if cash <= 0:
             return False
-        win = randint(0,1)  # TODO account for X's w/ dps in each scenario
+        win = randint(0,1)  # TODO account for X's, maybe w/ dps in each scenario
         if win:
             cash += (self.multiplier * self.stake_amount * self.tax_rate)
             self.multiplier = 1
@@ -47,11 +61,8 @@ class StakingSimulation(object):
         return self._run_sim_helper(cash, target)
 
     def run_sim(self):
-        # TODO make this error out better
-        cash = int(input('Starting cash (in million gp): '))
-        assert cash, "Please enter a number for cash"
-        target = int(input('Target cash (in million gp): '))
-        assert target, "Please enter a number for target"
+        cash = StakingSimulation._get_input_or_default('Starting cash (in million gp): ')
+        target = StakingSimulation._get_input_or_default('Target cash (in million gp): ')
         
         print (f"Running sim {self.sim_length} times with a base stake of {self.stake_amount}")
         for _ in range(self.sim_length):
